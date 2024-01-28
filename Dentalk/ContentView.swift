96328d57c5fcd4797f786477d338a44d589d62ed
Noah Fuery <nfuery@chapman.edu>
2024-01-27 18:05:51 -0800

    Initial Commit

---
+++ Dentalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dentalk
+//
+//  Created by Noah Fuery on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dentalk/DentalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DentalkApp.swift
+//  Dentalk
+//
+//  Created by Noah Fuery on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DentalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
